853d9aef846014ad65505842e9273d3594f0c120
Diogo Silva <83142137+D7lva@users.noreply.github.com>
2022-08-23 23:30:23 +0100

    Add files via upload

---
+++ aula 11 condições em js.js
@@ -0,0 +1,17 @@
+var vel = 78.2
+console.log('A velocidade do seu carro é ${vel}hm/h')
+
+console.log('dirija em segurança e sempre a usar o sinto de segurança')
+if (vel > 60) {
+    console.log('você ultrapassou o limite de velocidade sera, MULTADA!')
+
+//}
+var pais = 'França'
+console.log('vivendo em ${pais}')
+if('pais != brazil'){
+   console.log('brazileiro ') 
+
+   } else {
+    console.log('estrangeiro')
+    
+}
